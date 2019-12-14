4191002a58b58a3d1c9201d2024be14a24a47fae
lesrose <1178211058@qq.com>
2019-12-14 11:42:22 +0800

    modify test

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls home.")
